9a1c8313b50433267250d1b8d130720b72ecdabf
Subil <subil1407@gmail.com>
2018-07-08 21:12:18 -0400

    added the first screen with list items and checkboxes

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'YATODO',
-        theme: new ThemeData(
-        primarySwatch: Colors.black,
+      title: 'YATODO',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
       home: new TodoList(),
     );
@@ -17,35 +17,65 @@ class MyApp extends StatelessWidget {
 }
 
 class TodoList extends StatefulWidget {
-
   @override
   _TodoListState createState() => new _TodoListState();
 }
 
 class _TodoListState extends State<TodoList> {
-  int _listItems = [];
-
+  final _listItems = <String>['item1', 'item2', 'item3'];
+  final _completed = <String>[];
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Yet Another Todo List'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _deleteAllItems)
-        ],
-      ),
-      body: _buildTodoList(),
-      floatingActionButton: FloatingActionButton(
-          onPressed: _addItem,
+        appBar: new AppBar(
+          title: new Text('Yet Another Todo List'),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.delete_sweep), onPressed: _deleteAllItems)
+          ],
+        ),
+        body: _buildTodoList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _addNewItem,
           tooltip: 'new Item',
           child: Icon(Icons.add),
-                                                )
-    );
+        ));
   }
 
   Widget _buildTodoList() {
-    // code for the todolist layout (use list builder)
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i < _listItems.length) {
+          return _buildRow(_listItems[i]);
+        }
+      },
+    );
+  }
+
+  Widget _buildRow(String item) {
+    final alreadyDone = _completed.contains(item);
+    return new ListTile(
+      title: new Text(
+        item,
+      ),
+      leading: new Icon(
+        alreadyDone ? Icons.check_circle : Icons.check_circle_outline,
+        color: alreadyDone ? Colors.green : null,
+      ),
+      onTap: () {
+        setState(
+          () {
+            if (alreadyDone) {
+              _completed.remove(item);
+            } else {
+              _completed.add(item);
+            }
+          },
+        );
+      },
+    );
   }
 
   void _deleteAllItems() {
@@ -58,7 +88,7 @@ class _TodoListState extends State<TodoList> {
     // activated by long press on the item in list
   }
 
-  void _newItem() {
+  void _addNewItem() {
     // insert navigator to open the page to add new item
   }
 }
