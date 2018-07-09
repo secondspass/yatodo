103b9bd14640968570c8826c593b278d4fb2ece5
Subil <subil1407@gmail.com>
2018-07-09 15:33:07 -0400

    Added ability to add items, delete items and clear all items

--- lib/main.dart
+++ lib/main.dart
@@ -75,20 +75,61 @@ class _TodoListState extends State<TodoList> {
           },
         );
       },
+      onLongPress: _deleteItem(item, alreadyDone),
     );
   }
 
   void _deleteAllItems() {
-    // code to remove all todo items and clear the screen
-    // attached to 'x' icon on appbar
+    setState(() {
+      _listItems.clear();
+      _completed.clear();
+    });
   }
 
-  void _deleteItem() {
+  void _deleteItem(String item, bool alreadyDone) {
     // deletes a single item in todo list
     // activated by long press on the item in list
+    // returns an anonymous function with item in closure
+    return () {
+      setState(() {
+        _listItems.remove(item);
+        if (alreadyDone) {
+          _completed.remove(item);
+        }
+      });
+    };
   }
 
   void _addNewItem() {
     // insert navigator to open the page to add new item
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+            final itemEntryField = new TextField(controller: new TextEditingController());
+          final addButton = new RaisedButton(
+              child: Text('ADD ITEM'),
+              onPressed: () {
+                setState(() {
+                  String itemText = itemEntryField.controller.text;
+                  if ((itemText != '') || !_listItems.contains(itemText)) {
+                    _listItems.add(itemText);
+                    itemEntryField.controller.text = '';
+                  }
+                });
+              });
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Add new item'),
+            ),
+            body: new Column(children: <Widget>[
+              itemEntryField,
+              addButton,
+              new Text('Press back button to go back to list')
+            ]),
+          );
+        },
+      ),
+    );
   }
 }
